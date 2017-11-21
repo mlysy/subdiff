7d62039a39f71177693d0afe6168cc93d95c5814
mlysy <mlysy@users.noreply.github.com>
2017-11-21 15:29:11 -0500

    added cochranQ

---
+++ R/cochranQ.R
@@ -0,0 +1,15 @@
+#' Cochran's Q-test for heterogeneity.
+#'
+#' Under \code{H0}, \code{k} independent estimators are of a common parameter; their precision-weighted sum-of-squares follows asymptotically a chi^2 distribution with \code{k-1} degrees of freedom.
+#'
+#' @param est A vector of independent estimates of a common parameter.
+#' @param se A corresponding vector of standard errors.
+#' @return The value of the test statistic, its p-value, and the weighted mean.
+#' @export
+cochranQ <- function(est, se) {
+  k <- length(est)
+  wgt <- 1/se^2
+  ebar <- sum(wgt * est)/sum(wgt)
+  Q <- sum(wgt*(est - ebar)^2)
+  c(Q = Q, pval = pchisq(Q, df = k-1, lower.tail = FALSE), ebar = ebar)
+}
