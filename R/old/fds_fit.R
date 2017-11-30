8f0903cf1ab17ce33d6ad040c7cedc3b371c7f5c
cruiseling <cruiseling@gmail.com>
2017-11-30 15:09:27 -0500

    remove ds.avg from fds_fit

--- R/old/fds_fit.R
+++ R/old/fds_fit.R
@@ -3,7 +3,7 @@
 #' @param dX one or two-column matrix of trajectory increments.
 #' @param dT Interobservation time.
 #' @param type type of downsampling method, "naive" means using basic downsampling method, "comp"
-#' means using composite downsampling method
+#' means using composite downsampling method, "avg" means using average downsampling method
 #' @param Tz Optional Toeplitz matrix for intermediate calculations.
 #' @param var_calc If \code{TRUE}, also estimate variance matrix.
 #' @param ... Additional \code{control} arguments to \code{stats::optim}.
@@ -14,13 +14,14 @@
 fds_fit <- function(dX, dT, type = "naive", Tz, ds, var_calc = TRUE) {
   Xt <- apply(rbind(0, dX), 2, cumsum)
   switch(type, 
-         naive = .ds.naive(Xt, dT, Tz, ds, var_calc), 
-         comp = .ds.comp(Xt, dT, Tz, ds, var_calc))
+         naive = .ds.naive(Xt, dT, Tz, ds, var_calc = var_calc), 
+         comp = .ds.comp(Xt, dT, Tz, ds, var_calc), 
+         avg = .ds.avg(Xt, dT, Tz, ds, var_calc))
 }
 
-.ds.naive <- function(Xt, dT, Tz, ds, var_calc) {
-  # downsampling
-  Xt <- downSample(Xt, ds)
+.ds.naive <- function(Xt, dT, Tz, ds, pos = 1, var_calc) {
+  # downsampling at position pos
+  Xt <- downSample(Xt, ds, pos)
   dX <- apply(Xt, 2, diff)
   # memory allocation and change interobservation time
   N <- nrow(dX)
@@ -62,6 +63,38 @@ fds_fit <- function(dX, dT, type = "naive", Tz, ds, var_calc = TRUE) {
     V_hat <- solveV(-V_hat)
     colnames(V_hat) <- theta_names
     rownames(V_hat) <- theta_names
+    ans <- list(coef = theta_hat, vcov = V_hat)
+  }
+  ans
+}
+
+.ds.avg <- function(Xt, dT, Tz, ds, var_calc) {
+  # memory allocation
+  N <- nrow(Xt)
+  q <- ncol(Xt)
+  N_ds <- floor(N/ds) - 1
+  nq <- if(q == 1) 1 else 3
+  ntheta <- 1+q+nq
+  theta_hat <- rep(0, ntheta)
+  theta_names <- c("gamma", paste0("mu", 1:q), paste0("lambda", 1:nq))
+  if(missing(Tz)) Tz <- Toeplitz(n = N_ds)
+  # inference
+  if(!var_calc) {
+    for(ii in 1:ds) {
+      theta_hat <- theta_hat + .ds.naive(Xt, dT, Tz, ds, pos = ii, var_calc)  
+    }
+    ans <- theta_hat / ds
+  } else {
+    V_hat <- matrix(0, ntheta, ntheta)
+    colnames(V_hat) <- theta_names
+    rownames(V_hat) <- theta_names
+    for(ii in 1:ds) {
+      tmp <- .ds.naive(Xt, dT, Tz, ds, pos = ii, var_calc)
+      theta_hat <- theta_hat + tmp$coef
+      V_hat <- V_hat + tmp$vcov
+    }
+    theta_hat <- theta_hat / ds
+    V_hat <- V_hat / ds^2
     ans <- list(coef = theta_hat, vcov = V_hat)
   }
   ans
