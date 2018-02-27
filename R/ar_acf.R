d147ced167d694910135e001ac811988c657a00f
cruiseling <cruiseling@gmail.com>
2018-02-27 11:43:58 -0500

    add acf-function for ar and ma model

---
+++ R/ar_acf.R
@@ -0,0 +1,29 @@
+#' ACF of the Autoregressive Model
+#' @param alpha
+#' @param rho parameter of MA model
+#' @param dT
+#' @param N
+#' @return vector of length N
+#' @details 
+#' Yt = (1-rho) Yt-1 + rho Xt, where Xt is fBM process with parameter alpha
+#' @export
+ar_acf <- function(alpha, rho, dT, N, nlim = 1e3) {
+  rho1 <- 1-rho
+  acf1 <- fbm_acf(alpha, dT, N+nlim)
+  acf2 <- rep(NA, N)
+  for(ii in 1:N) {
+    acf2[ii] <- .ar_terms(acf1, rho1, ii-1, nlim)
+  }
+  acf2 * rho^2
+}
+
+#' nested function
+.ar_terms <- function(acf1, rho1, k, nlim) {
+  mat.temp <- matrix(NA, nlim, nlim)
+  for(ii in 1:nlim-1) {
+    for(jj in 1:nlim-1) {
+      mat.temp[ii+1, jj+1] <- rho1^(ii+jj) * acf1[abs(k+ii-jj)+1]
+    }
+  }
+  sum(mat.temp)
+}
---
+++ R/ma_acf.R
@@ -0,0 +1,40 @@
+#' ACF of the Moving Average Model
+#' @param alpha
+#' @param rho parameter of MA model
+#' @param dT
+#' @param N
+#' @return vector of length N
+#' @details 
+#' Yt = (1-rho) Xt + rho Xt-1, where Xt is fBM process with parameter alpha
+#' @export
+ma_acf <- function(alpha, rho, dT, N) {
+  acf1 <- fbm_acf(alpha, dT, N+1)
+  acf2 <- rep(NA, N)
+  rho1 <- 1 - rho
+  acf2[1] <- (rho1^2 +rho^2) * acf1[1] + 2*rho*rho1*acf1[2]
+  acf2[-1] <- (rho1^2 +rho^2) * acf1[-c(1,N+1)] + rho*rho1*(acf1[-c(N,N+1)]+acf1[-c(1,2)])
+  acf2
+}
+
+
+# # for ma, Yt = (1-rho) Xt + rho Xt-1
+# mat_func <- function(rho, N) {
+#   mat <- matrix(0, N, N+1)
+#   for(ii in 1:N) {
+#     mat[ii, ii] <- 1 - rho
+#     mat[ii, ii+1] <- rho
+#   }
+#   mat
+# }
+# 
+# # test
+# rho <- .2
+# N <- 5
+# m1 <- mat_func(rho, N)
+# acf1 <- rnorm(N+1)
+# m1 %*% toeplitz(acf1) %*% t(m1)
+# 
+# # 
+# 
+# range(toeplitz(ar_acf(acf1, rho)) - m1 %*% toeplitz(acf1) %*% t(m1))
+
