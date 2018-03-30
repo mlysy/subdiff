86b1721d6e6a3b24a64ad9c44ad402de973d417c
cruiseling <cruiseling@gmail.com>
2018-03-30 03:24:17 -0400

    truncated fdy and fma

--- R/fdl_fit.R
+++ R/fdl_fit.R
@@ -213,3 +213,32 @@ flo_fit <- function(dX, dT, Tz, var_calc = TRUE, ...) {
 # p = ilogit(eta) = 1/(1+exp(eta))
 # 1-p = exp(eta)/(1+exp(eta)) * exp(-eta)/exp(-eta) = 1/(exp(-eta)+1)
 # => pen(eta) = log(1) - log(1+exp(eta)) + log(1) - log(1+exp(-eta))
+
+#' @rdname fdl_fit
+#' @export
+# truncted estimation function for fdy
+fdy_trunc <- function(dX, dT, tau) {
+  # memory allocation
+  N <- nrow(dX)
+  q <- ncol(dX)
+  theta_hat <- rep(NA, 1)
+  theta_names <- c("gamma")
+  Tz <- Toeplitz(n = N)
+  # profile likelihood on regular scale
+  ll.prof <- function(theta) {
+    alpha <- itrans_alpha(theta)
+    Tz$setAcf(fdyn_acf(alpha, tau, dT, N))
+    suff <- lmn.suff(Y = dX, X = dT, acf = Tz)
+    lmn.prof(suff)
+  }
+  # calculate MLE
+  theta_hat <- optimize(f = ll.prof,
+                        interval = c(-5.3, -0.02), maximum = TRUE)$maximum
+  names(theta_hat) <- theta_names
+  # variance estimate
+  V_hat <- hessian(ll.prof, x = theta_hat)
+  V_hat <- subdiff:::solveV(-V_hat)
+  colnames(V_hat) <- theta_names
+  rownames(V_hat) <- theta_names
+  list(coef = theta_hat, vcov = V_hat)
+}
